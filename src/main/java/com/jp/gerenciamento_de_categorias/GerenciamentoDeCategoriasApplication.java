5565a273b64aef07b3a885130d41c585829cb489
João Paulo <joaopaulotraguetta14@gmail.com>
2026-03-21 17:29:49 -0300

    .

---
+++ src/main/java/com/jp/gerenciamento_de_categorias/GerenciamentoDeCategoriasApplication.java
@@ -0,0 +1,13 @@
+package com.jp.gerenciamento_de_categorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeCategoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeCategoriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/gerenciamento_de_categorias/GerenciamentoDeCategoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.gerenciamento_de_categorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeCategoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
